2fd78d96ed224aa6f197f9eed766cc8753e6f7c0
Renato <u201511892@upc.edu.pe>
2022-06-16 21:46:15 -0500

    fix: person attributes

--- WebApplication1/WebApplication1/App/Domain/Models/Person.cs
+++ WebApplication1/WebApplication1/App/Domain/Models/Person.cs
@@ -5,7 +5,5 @@ public class Person
     public int Id { get; set; }
     public string Phone{ get; set; }
     public User User { get; set; }
-    public int UserId { get; set; }
     public Courier Courier { get; set; }
-    public int CourierId { get; set; }
 }
--- WebApplication1/WebApplication1/App/Resources/PersonResource.cs
+++ WebApplication1/WebApplication1/App/Resources/PersonResource.cs
@@ -4,6 +4,4 @@ public class PersonResource
 {
     public int Id { get; set; }
     public string Phone{ get; set; }
-    public int UserId { get; set; }
-    public int CourierId { get; set; }
 }
